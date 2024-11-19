11d66c6df8fde34ab9465f85ac815b341acd425a
abdullah-bridge-i <abdullah.m@bridge-i.asia>
2024-11-19 18:34:50 +0800

    Initiall nuxt 3 project with typescript and ESlint

---
+++ components/TestComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+defineProps<{ message: string }>();
+
+const count = ref<number>(0);
+
+function increment() {
+  count.value++; // Correct type
+}
+
+
+</script>
+
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="increment">Increment: {{ count }}</button>
+  </div>
+</template>
