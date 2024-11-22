a65c8a7625538d4696e9ebbc1adbf5a6a34006c6
abdullah-bridge-i <abdullah.m@bridge-i.asia>
2024-11-22 12:25:12 +0800

    Add home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ const config: NuxtConfig = {
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  ssr: true,
   devtools: { enabled: true },
   typescript: {
     strict: true,
